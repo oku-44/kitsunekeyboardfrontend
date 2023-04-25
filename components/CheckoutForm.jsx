5bad11ac1e3daa4914c8bf26759bf111a8516707
oku <1004oku1004@gmail.com>
2023-04-25 12:29:18 +0900

    edit cartdetail type

--- components/CheckoutForm.jsx
+++ components/CheckoutForm.jsx
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
 			elements,
 			confirmParams: {
 				// Make sure to change this to your payment completion page
-				return_url: "http://localhost:3000/products",
+				return_url: "/products",
 			},
 		});
 
