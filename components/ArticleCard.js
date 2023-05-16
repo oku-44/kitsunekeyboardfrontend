6bf87d46f15ffa9a0e345eb0fb718141faccc48b
oku <1004oku1004@gmail.com>
2023-05-16 15:18:42 +0900

    fix articlecard top image

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -15,7 +15,7 @@ export default function ArticleCard({ article }) {
         >
           <div className="relative w-full">
             <img
-              src={getStrapiURL(article.attributes.image.data.attributes.url)}
+              src={getStrapiURL(article.attributes.image.data.attributes.url[0])}
               alt=""
               className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1]"
             />
