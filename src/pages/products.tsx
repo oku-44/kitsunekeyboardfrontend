86b88913e6715099a3be777918ad5129faf483ab
oku <1004oku1004@gmail.com>
2023-07-08 20:55:02 +0900

    fix products.tsx to remove unused components

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -54,12 +54,7 @@ const Products = ({ products }: ProductsProps) => {
 				<div className="py-8 lg:mx-auto lg:max-w-7xl lg:px-8">
 					<div className="flex items-center justify-between px-4 sm:px-6 lg:px-0">
 						<h2 className="text-2xl font-bold tracking-tight text-gray-900">全ての商品</h2>
-						{/* <Link href="#" className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block">
-							全ての商品を見る
-							<span aria-hidden="true"> &rarr;</span>
-						</Link> */}
 					</div>
-
 					<div className="relative mt-8">
 						<div className="relative -mb-6 w-full overflow-x-auto pb-6">
 							<ul
@@ -142,13 +137,6 @@ const Products = ({ products }: ProductsProps) => {
 								))}
 							</ul>
 						</div>
-					</div>
-
-					<div className="mt-12 flex px-4 sm:hidden justify-end">
-						<Link href="/products" className="text-sm font-semibold text-indigo-600 hover:text-indigo-500">
-							全ての商品を見る
-							<span aria-hidden="true"> &rarr;</span>
-						</Link>
 					</div>
 				</div>
 			</div>
